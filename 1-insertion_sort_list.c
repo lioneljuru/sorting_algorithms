dabe04122241b69db66f97417c9a5b01bacb9d78
lioneljuru <lioneljuru@gmail.com>
2024-01-23 22:18:42 +0200

    update

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,7 +12,7 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 	if (n2->next != NULL)
 		n2->next->prev = *n1;
 	n2->prev = (*n1)->prev;
-	n2->prev = *n1;
+	n2->next = *n1;
 	if ((*n1)->prev != NULL)
 		(*n1)->prev->next = n2;
 	else
